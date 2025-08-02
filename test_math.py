42a3b7061e378360a81eca3c8d752c19a4100c5b
khan-walker <khanwalkergithub@gmail.com>
2025-08-02 23:41:31 +0500

    Parameterized test cases

--- test_math.py
+++ test_math.py
@@ -16,3 +16,26 @@ def test_divide_by_zero():
         
     assert 'division by zero' in str(e.value)
     
+    
+    
+    
+# Multiplication test ideas:
+# two positive integers
+# identity: multiply by 1
+# zero: mulity by 0
+# posiiitive by negative
+# negative by negative
+# floats
+
+products  = [
+    (2, 3, 6),
+    (1, 99, 99),
+    (0, 99, 0), 
+    (3, -4, -12),
+    (-5, -5, 25),
+    (2.5, 6.7, 16.75)
+]
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+    assert a * b == product
