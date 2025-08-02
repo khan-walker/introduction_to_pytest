5febc7ed2353970c956dea3fd38dc8d9352cbb9d
khan-walker <khanwalkergithub@gmail.com>
2025-08-02 22:28:59 +0500

    A test case with an exception

--- test_math.py
+++ test_math.py
@@ -1,3 +1,4 @@
+import pytest
 
 def test_one_plus_one():
     assert 1 + 1 == 2
@@ -6,5 +7,12 @@ def test_one_plus_one():
 def test_one_plus_two():
     a = 1
     b = 2
-    c = 0
+    c = 3
     assert a + b == c
+    
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+        
+    assert 'division by zero' in str(e.value)
+    
