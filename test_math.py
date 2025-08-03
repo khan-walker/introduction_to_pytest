5a743e85f653a29512b6cd5dbf01ed80fcb2c285
khan-walker <khanwalkergithub@gmail.com>
2025-08-03 20:50:29 +0500

    Filtering tests

--- test_math.py
+++ test_math.py
@@ -1,5 +1,8 @@
 import pytest
 
+
+
+@pytest.mark.math
 def test_one_plus_one():
     assert 1 + 1 == 2
     
@@ -36,6 +39,7 @@ products  = [
     (2.5, 6.7, 16.75)
 ]
 
+@pytest.mark.math
 @pytest.mark.parametrize('a, b, product', products)
 def test_multiplication(a, b, product):
     assert a * b == product
